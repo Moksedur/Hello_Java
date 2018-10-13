a8423c63a0561c16351b23793aab718abffec366
Moksedur <moksedur_rahman@yahoo.com>
2018-10-13 23:29:32 +0600

    Hello java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    /**
+     * @Hello Java
+     */
+    public static void main(String[] args) {
+        // Print Hello Java
+        System.out.println("Hello Java");
+    }
+    
+}
